74882efa8083a90802c41475a1906f9ea9da2a0d
Chak <chaklook11@gmail.com>
2025-03-08 05:14:06 +0100

    Оптимізував шрифт, css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { ThemeProvider } from 'next-themes';
 import { useEffect, useState } from 'react';
+import Script from 'next/script';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 import nextI18NextConfig from '../../next-i18next.config.js';
@@ -23,6 +24,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       <Layout
         title={pageProps.title || 'IN-FOMO'}
         description={pageProps.description || ''}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,25 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@900&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          href="/fonts/SofiaSans-Black.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              @font-face {
+                font-family: 'Sofia Sans';
+                font-style: normal;
+                font-weight: 900;
+                font-display: swap;
+                src: url('/fonts/SofiaSans-Black.ttf') format('truetype');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+              }
+            `,
+          }}
         />
       </Head>
       <body>
