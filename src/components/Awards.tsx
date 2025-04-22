7fcae47a845c4b8346e75e4ea1d31e33efec23ee
Chak <chaklook11@gmail.com>
2025-04-22 23:32:14 +0400

    5 stars

--- src/components/Awards.tsx
+++ src/components/Awards.tsx
@@ -5,7 +5,7 @@
  * Отображает награды в виде горизонтальной бегущей строки с эффектом бесконечной прокрутки.
  * Поддерживает мультиязычность через систему i18n (next-i18next).
  * 
- * @version 2.3.0
+ * @version 2.2.0
  * @author IN-FOMO Team
  */
 
@@ -188,17 +188,25 @@ const Awards = memo(() => {
                 </svg>
               </a>
               
-              {/* Добавляем стикер Clutch вместо звезд */}
-              <div 
-                className="clutch-widget" 
-                data-url="https://widget.clutch.co" 
-                data-widget-type="1" 
-                data-height="40" 
-                data-nofollow="true" 
-                data-expandifr="true" 
-                data-scale="100" 
-                data-clutchcompany-id="2459746"
-              ></div>
+              {/* Добавляем 5 звездочек с ссылкой на Clutch */}
+              <a 
+                href="https://clutch.co/profile/fomo-0#reviews" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="inline-flex items-center gap-1" 
+                title={hasTranslations ? t('clutch_reviews') || "View our Clutch reviews" : "Посмотреть отзывы на Clutch"}
+              >
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <svg 
+                    key={star} 
+                    className="w-5 h-5 text-yellow-400" 
+                    fill="currentColor" 
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                ))}
+              </a>
             </div>
           </div>
           
@@ -258,6 +266,12 @@ const Awards = memo(() => {
                 data-scale="80" 
                 data-clutchcompany-id="2459746">
               </div>
+              {/* Скрипт для загрузки виджета Clutch */}
+              <Script 
+                type="text/javascript" 
+                src="https://widget.clutch.co/static/js/widget.js" 
+                strategy="afterInteractive"
+              />
             </div>
           </div>
         </div>
@@ -336,13 +350,6 @@ const Awards = memo(() => {
           overflow: hidden;
         }
       `}</style>
-    
-      {/* Скрипт для загрузки виджета Clutch */}
-      <Script 
-        type="text/javascript" 
-        src="https://widget.clutch.co/static/js/widget.js" 
-        strategy="afterInteractive"
-      />
     </section>
   );
 });
